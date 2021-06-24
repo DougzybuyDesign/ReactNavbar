979e64e4e29853c7060de341b11dc3c71f835bff
Brian Douglas <70322945+DougzybuyDesign@users.noreply.github.com>
2021-06-24 18:12:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar/Nabar';
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>DougzybuyDesign</h1>
+      <h2>React fullstack devlopers</h2>
+    </div>
+  );
+}
